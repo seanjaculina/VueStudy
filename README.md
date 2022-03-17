c8e3097c77aeb216095d0428cf6acdf988adbbae
seanjaculina <seanjaculina1@gmail.com>
2022-03-17 14:23:14 -0700

    two components added, Header and Button. Also additional information to readme notes

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # StudyVue
 
 1) Used CLI - 'vue create vue-crashcourse'
+
+-- Personal Notes --
+
+2) App.vue
+    - Template is the output/HTML
+    - script is the logic
+    - style is the styling for the page
+
+3) To use a component, must be defined within the components object. Then may be passed to the template to be displayed.
+
+ 
+4) scoped within the tag means this is only for this 
+specific components scope 
---
+++ vue-crashcourse/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+          <Button text="Add Task" color="green"/>
+          <Button text="Update Task" color="blue"/>
+          <Button text="Delete Task" color="red"/>
+    </header>
+</template>
+
+
+<script>
+import Button from './Button'
+
+export default{
+    name: 'Header',
+    components:{
+        Button,
+    },
+        props: {
+        title: String, 
+    }
+}
+
+</script>
+
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
