3d4cb8b888ce51375050e7599716d428a3ebe75e
seanjaculina <seanjaculina1@gmail.com>
2022-03-23 14:22:54 -0700

    changed font awesome icon to color red

--- vue-crashcourse/src/components/Task.vue
+++ vue-crashcourse/src/components/Task.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="task">  
-    <h3>{{task.text}}</h3>
+    <h3>{{task.text}}
+        <i class="fas fa-times"></i>
+
+    </h3>
     <p>{{task.day}}</p>
     </div>
   
@@ -17,7 +20,9 @@ export default{
 </script>
 
 <style>
-
+.fas{
+    color: red;
+}
 .task {
   background: #f4f4f4;
   margin: 5px;
